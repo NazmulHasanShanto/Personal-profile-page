f50d03d0bf0cce0ea15d701cac849d65d199c204
Nazmul Hasan Shanto <129149922+NazmulHasanShanto@users.noreply.github.com>
2024-08-16 19:40:02 +0600

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -196,7 +196,7 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
 (function () {
     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
     s1.async = true;
-    s1.src = 'https://embed.tawk.to/66bee6880cca4f8a7a769960/default';
+    s1.src = 'https://embed.tawk.to/66bef05f146b7af4a43b071b/default';
     s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
