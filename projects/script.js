dc4219bbd2778d2f41748f7cd82bd1c70a31ea39
Nazmul Hasan Shanto <129149922+NazmulHasanShanto@users.noreply.github.com>
2024-08-19 10:00:50 +0600

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -24,7 +24,7 @@ document.addEventListener('visibilitychange',
             $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
-            document.title = "Come Back To Portfolio";
+            document.title = "Come Back To Profile";
             $("#favicon").attr("href", "/assets/images/favhand.png");
         }
     });
@@ -47,7 +47,7 @@ function showProjects(projects) {
         projectsHTML += `
         <div class="grid-item ${project.category}">
         <div class="box tilt" style="width: 380px; margin: 1rem">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <img draggable="false" src="img/${project.image}.png" alt="project" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
